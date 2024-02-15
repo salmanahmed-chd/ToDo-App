417b8617d2dbabfb1544a8be519cb09ef6732509
Salman Ahmed <ahmedpec1005@gmail.com>
2024-02-15 23:48:59 +0530

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -24,7 +24,7 @@ const TaskItem = ({ item, items, index, setItems, completed, setCompleted }) => 
         <div className="bg-gray-100 px-3 py-2 rounded-md shadow-md mt-3 flex justify-between items-center">
             <div className="">
                 <h2 className='mb-2 font-bold'>{title[0].toUpperCase()}{title.substring(1)}</h2>
-                <h3 className='ml-2'>{description[0].toUpperCase()}{description.substring(1)}</h3>
+                <h3 className='ml-2'>{description ? description[0].toUpperCase() + description.substring(1) : ''}</h3>
             </div>
             <div className="mr-4">
                 <button className='mr-2' onClick={handleRemoveItem}><i className="ri-delete-bin-5-line text-2xl hover:text-red-500"></i></button>
